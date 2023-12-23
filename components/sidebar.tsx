79abdcd0e3148975cf53709018b66ba3644a2229
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-12-23 13:57:37 +0530

    Refactor sidebar component to use Link component for navigation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -14,20 +14,24 @@ export default function Sidebar() {
   return (
     <div className="border-r h-full w-full">
       <div className="space-y-4 p-4">
-        <div className="px-3 py-2 space-y-1">
-          <Button className="w-full">
-            <FaUserFriends className="mr-2 h-4 w-4" /> Add a new friend
-          </Button>
-          <Button className="w-full">
-            <BiNotification className="mr-2 h-4 w-4" /> Friend Requests ( 2 )
-          </Button>
+        <div className="px-3 py-2 flex flex-col gap-y-2">
+          <Link href="/add-friend">
+            <Button className="w-full">
+              <FaUserFriends className="mr-2 h-4 w-4" /> Add a new friend
+            </Button>
+          </Link>
+          <Link href="/friend-requests">
+            <Button className="w-full">
+              <BiNotification className="mr-2 h-4 w-4" /> Friend Requests ( 2 )
+            </Button>
+          </Link>
         </div>
         <div className="px-3 py-2">
           <h2 className="mb-2 py-4 text-lg font-semibold tracking-tight">
             Friends
           </h2>
           <div className="space-y-1">
-            <Link href="/chat/1231231">
+            <Link href="/chat/6c8d370a-8dd4-49fc-9338-f70afb66140f">
               <ContextMenu>
                 <ContextMenuTrigger asChild>
                   <Button variant="secondary" className="w-full justify-start">
@@ -40,7 +44,7 @@ export default function Sidebar() {
               </ContextMenu>
             </Link>
 
-            <Link href="/chat/1231231">
+            <Link href="/chat/855849be-fb3f-4b94-a290-2b95a222aa66">
               <ContextMenu>
                 <ContextMenuTrigger asChild>
                   <Button variant="ghost" className="w-full justify-start">
