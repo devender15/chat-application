95ac5f45d85a2f36f2dbc300cf0f6db098e6ae54
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-12-31 00:25:02 +0530

    Remove unnecessary rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
 const nextConfig = {
   images: {
     domains: ["www.pexels.com", "img.icons8.com", "lh3.googleusercontent.com", "uploadthing.com"],
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-    ];
-  },
+  }
 };
 
 module.exports = nextConfig;
