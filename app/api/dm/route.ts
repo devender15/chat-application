87d2245cbe04081da9cbb0ca001dc091877b62db
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-01-10 15:51:45 +0530

    Commented out unused code in route.ts

--- app/api/dm/route.ts
+++ app/api/dm/route.ts
@@ -9,72 +9,72 @@ const MESSAGES_BATCH = 10;
 
 export async function GET(req: Request) {
     try {
-        const profile = await currentProfile();
-        const { seachParams } = new URL(req.url);
+        // const profile = await currentProfile();
+        // const { seachParams } = new URL(req.url);
 
-        const cursor = searchParams.get("cursor");
-        const conversationId = searchParams.get("conversationId");
+        // const cursor = searchParams.get("cursor");
+        // const conversationId = searchParams.get("conversationId");
 
-        if(!profile) {
-            return new NextResponse("Unauthorized", { status: 401 });
-        }
+        // if(!profile) {
+        //     return new NextResponse("Unauthorized", { status: 401 });
+        // }
 
-        if(!conversationId) {
-            return new NextResponse("Conversation Id is missing!", { status: 400 });
-        }
+        // if(!conversationId) {
+        //     return new NextResponse("Conversation Id is missing!", { status: 400 });
+        // }
 
-        let messages: DirectMessage[] = [];
+        // let messages: DirectMessage[] = [];
 
-        if(cursor) {
-            messages = await db.directMessage.findMany({
-                take: MESSAGES_BATCH,
-                skip: 1,
-                cursor: {
-                    id: cursor,
-                },
-                where: {
-                    conversationId,
-                },
-                include: {
-                    member: {
-                        include: {
-                            profile: true,
-                        }
-                    }
-                },
-                orderBy: {
-                    createdAt: "desc",
-                }
-            })
-        } else {
-            messages = await db.directMessage.findMany({
-                take: MESSAGES_BATCH,,
-                where: {
-                    conversationId,
-                },
-                include: {
-                    member: {
-                        include: {
-                            profile: true,
-                        }
-                    }
-                },
-                orderBy: {
-                    createdAt: "desc"
-                }
-            })
-        }
+        // if(cursor) {
+        //     messages = await db.directMessage.findMany({
+        //         take: MESSAGES_BATCH,
+        //         skip: 1,
+        //         cursor: {
+        //             id: cursor,
+        //         },
+        //         where: {
+        //             conversationId,
+        //         },
+        //         include: {
+        //             member: {
+        //                 include: {
+        //                     profile: true,
+        //                 }
+        //             }
+        //         },
+        //         orderBy: {
+        //             createdAt: "desc",
+        //         }
+        //     })
+        // } else {
+        //     messages = await db.directMessage.findMany({
+        //         take: MESSAGES_BATCH,,
+        //         where: {
+        //             conversationId,
+        //         },
+        //         include: {
+        //             member: {
+        //                 include: {
+        //                     profile: true,
+        //                 }
+        //             }
+        //         },
+        //         orderBy: {
+        //             createdAt: "desc"
+        //         }
+        //     })
+        // }
 
-        let nextCursor = null;
+        // let nextCursor = null;
 
-        if(messages.length === MESSAGES_BATCH) {
-            nextCursor = messages[MESSAGES_BATCH - 1].id;
-        }
+        // if(messages.length === MESSAGES_BATCH) {
+        //     nextCursor = messages[MESSAGES_BATCH - 1].id;
+        // }
 
-        return NextResponse.json({
-            items: messages,
-            nextCursor
-        });
+        // return NextResponse.json({
+        //     items: messages,
+        //     nextCursor
+        // });
     } catch (err) {
         console.log(err);
         return new NextResponse("Internal error", { status: 500 });
